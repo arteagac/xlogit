e4f7490263d34040cfb60f372092c56cfcf7205d
Cristian Arteaga <cristiandavidarteaga@gmail.com>
2019-09-04 20:39:12 -0700

    Update MultinomialLogit.py

--- examples/MultinomialLogit.py
+++ examples/MultinomialLogit.py
@@ -1,16 +1,33 @@
-import pandas as pd
 from pymlogit.linear import MultinomialModel
 
+import pandas as pd
 df = pd.read_csv("examples/data/fishing_long.csv")
 
-X = df[['price']].values
+#============ With both, alternative specific and individual specific variables
+varnames = ['income','price']
+X = df[varnames].values
 y = df['choice'].values
 
 model = MultinomialModel()
-model.fit(X,y,varnames = ['income','price'],isvars = ['income'], asvars=['price'],alternatives=['beach','boat','charter','pier'])
+model.fit(X,y,isvars = ['income'], asvars=['price'],alternatives=['beach','boat','charter','pier'],varnames= varnames)
 model.summary()
 
+#============= With only alternative specific variables
+varnames = ['price']
+X = df[varnames].values
+y = df['choice'].values
 
 model = MultinomialModel()
-model.fit(X,y,varnames = ['price'],asvars = ['price'],alternatives=['beach','boat','charter','pier'],fit_intercept=False)
+model.fit(X,y,asvars=['price'],alternatives=['beach','boat','charter','pier'],varnames= varnames)
 model.summary()
+
+
+#=============== With only individual specific variables
+varnames = ['income']
+X = df[varnames].values
+y = df['choice'].values
+
+model = MultinomialModel()
+model.fit(X,y,isvars=['income'],alternatives=['beach','boat','charter','pier'],varnames= varnames)
+model.summary()
+

6c36c14d8138c5f5021cdaaa933e75c5cac3231d
Cristian Arteaga <cristiandavidarteaga@gmail.com>
2020-05-19 13:26:23 -0700

    Examples updated

--- examples/MultinomialLogit.py
+++ examples/MultinomialLogit.py
@@ -1,9 +1,9 @@
 from pymlogit.linear import MultinomialModel
 
 import pandas as pd
-df = pd.read_csv("examples/data/fishing_long.csv")
+df = pd.read_csv("fishing_long.csv")
 
-#============ With both, alternative specific and individual specific variables
+#============ Estimate model with both alternative specific and individual specific variables
 varnames = ['income','price']
 X = df[varnames].values
 y = df['choice'].values
@@ -12,17 +12,7 @@ model = MultinomialModel()
 model.fit(X,y,isvars = ['income'], asvars=['price'],alternatives=['beach','boat','charter','pier'],varnames= varnames)
 model.summary()
 
-#============= With only alternative specific variables
-varnames = ['price']
-X = df[varnames].values
-y = df['choice'].values
-
-model = MultinomialModel()
-model.fit(X,y,asvars=['price'],alternatives=['beach','boat','charter','pier'],varnames= varnames)
-model.summary()
-
-
-#=============== With only individual specific variables
+#============ Estimate model with only individual specific variables
 varnames = ['income']
 X = df[varnames].values
 y = df['choice'].values

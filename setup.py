cd54c01cc4c9282c03701db953020a7cd9bac315
Cristian Arteaga <cristiandavidarteaga@gmail.com>
2020-05-19 13:10:30 -0700

    importing error fixed

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-from setuptools import setup
+import setuptools
 
-setup(name='pymlogit',
-      version='0.0.1',
+setuptools.setup(name='pymlogit',
+      version='0.0.2',
       description='Estimation of discrete choice models in python',
       url='https://github.com/arteagac/pymlogit',
       author='Cristian Arteaga',
       author_email='cristiandavidarteaga@gmail.com',
       license='MIT',
       packages=['pymlogit'],
-      zip_safe=False)
+      zip_safe=False,
+      python_requires='>=3.5',
+      install_requires=[
+          'numpy>=1.13.1',
+          'scipy>=1.0.0'
+      ])

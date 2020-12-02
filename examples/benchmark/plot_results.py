8da4c68c480f2e7041396223c8d55c9a5707e36b
Cristian Arteaga <cristiandavidarteaga@gmail.com>
2020-12-02 14:17:54 -0800

    Benchmark results updated

--- examples/benchmark/plot_results.py
+++ examples/benchmark/plot_results.py
@@ -6,10 +6,8 @@ import pandas as pd
 matplotlib.rcParams.update({'font.size': 14,
                             'axes.spines.right': False,
                             'axes.spines.top': False})
-markers = ['s', '|', '^', 'x', 'o', '1']
-lines = [':', '--', '-']
-colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", '#8c564b']
-
+markers = ['d', 's', 'x', 'o', '^', '|']
+colors = ["#377eb8", "#4daf4a", "#ff7f00", "#e41a1c", "#984ea3",  "#a4c500"]
 
 # ==========================================
 # pylogit and mlogit benchmark
@@ -24,9 +22,9 @@ def plot_memory_benchmark(dataset):
     plt.figure()
     for i, lib in enumerate(libs):
         d = dfe[dfe.library == lib][["draws", "ram"]].values.T
-        plt.plot(d[0], d[1], marker=markers[i])
+        plt.plot(d[0], d[1], marker=markers[i], c=colors[i])
     d = dfe[dfe.library == "xlogit_gpu"][["draws", "gpu"]].values.T
-    plt.plot(d[0], d[1], marker="+", c="#bcbd22")
+    plt.plot(d[0], d[1], marker=markers[3], c=colors[3], linestyle="--")
     plt.legend([i + " (RAM)" for i in libs] + ["xlogit_gpu (GPU)"])
     plt.xlabel("Random draws")
     plt.ylabel("Memory usage (GB)")
@@ -40,7 +38,7 @@ def plot_time_benchmark(dataset):
     plt.figure()
     for i, lib in enumerate(libs):
         d = dfe[dfe.library == lib][["draws", "time"]].values.T
-        plt.plot(d[0], d[1], marker=markers[i])
+        plt.plot(d[0], d[1], marker=markers[i], c=colors[i])
     plt.legend(libs)
     plt.xlabel("Random draws")
     plt.ylabel("Time (Seconds)")
@@ -59,6 +57,7 @@ plot_time_benchmark("artificial")
 # ==========================================
 # apollo and biogeme benchmark
 # ==========================================
+lines = [':', '--', '-']
 r_draws = [100, 500, 1000, 1500]
 r_cores = [16, 32, 64]
 dfab = pd.read_csv("results/benchmark_results_apollo_biogeme.csv")
@@ -73,7 +72,7 @@ for lib in libs:
 
 
 def plot_time_benchmark_apollo_biogeme(df):
-    plt.figure(figsize=(11, 7))
+    plt.figure(figsize=(8, 7))
     for li, lib in enumerate(['apollo', 'biogeme']):
         for c, cores in enumerate(r_cores):
             idx = (df.library == lib) & (df.cores == cores)
@@ -81,10 +80,12 @@ def plot_time_benchmark_apollo_biogeme(df):
             plt.plot(d[0], d[1], marker=markers[-li - 1], linestyle=lines[c],
                      c=colors[-li - 1], label="{} {} cores".format(lib, cores))
     for li, lib in enumerate(libs):
-        idx = (df.library == lib)
-        d = df[idx][["draws", "time"]].values.T
-        plt.plot(d[0], d[1], marker=markers[li], c=colors[li], label=lib)
-    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.11), ncol=4)
+        if lib == "xlogit" or lib == "xlogit_gpu":
+            idx = (df.library == lib)
+            d = df[idx][["draws", "time"]].values.T
+            plt.plot(d[0], d[1], marker=markers[li], c=colors[li], label=lib)
+
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.11), ncol=3)
     plt.xlabel("Random draws")
     plt.ylabel("Time (Seconds)")
     plt.title("Estimation time (artificial dataset)")

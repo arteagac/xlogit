e1d8320c074cba164d7ba58816269f49a4a458a8
Cristian Arteaga <cristiandavidarteaga@gmail.com>
2020-12-18 22:20:25 -0800

    Docs updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('xlogit/xlogit'))
+sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../../../xlogit'))
 
 # -- Project information -----------------------------------------------------
@@ -55,3 +55,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
+
+html_theme_options = {
+    'collapse_navigation': False,
+}

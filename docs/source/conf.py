2474c6cf287f1c63d2d715a7e74f123689badfea
Cristian Arteaga <cristiandavidarteaga@gmail.com>
2020-12-18 22:03:27 -0800

    Docs updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+sys.path.insert(0, os.path.abspath('xlogit/xlogit'))
 sys.path.insert(0, os.path.abspath('../../../xlogit'))
 
 # -- Project information -----------------------------------------------------
